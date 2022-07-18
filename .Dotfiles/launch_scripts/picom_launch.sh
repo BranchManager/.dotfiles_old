c38187b8bc979d13a8daad0a9938cc0e0fe589fd
BranchManager <nbranch@vols.utk.edu>
2022-07-17 23:05:56 -0400

    removing dotfiles origianl

--- .Dotfiles/launch_scripts/picom_launch.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-
-#LOCATION ON MY MACHINE: /home/branchmanager/launch_scripts/picom_launch.sh
-killall -q picom
-
-while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
-
-/home/branchmanager/.config/awesome/picom/build/src/picom --experimental-backends -b | tee 2>&1 /home/branchmanager/.Dotfiles/logs/picom.log
