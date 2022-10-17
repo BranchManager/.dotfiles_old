2aa106fa16c0f5f9fb8ee2455a6313f182f695f1
BranchManager <nbranch@vols.utk.edu>
2022-10-16 23:03:39 -0400

    more work on notif

--- .config/eww/scripts/notif.py
+++ .config/eww/scripts/notif.py
@@ -2,6 +2,9 @@ import subprocess as subp
 import requests
 import json
 import sys
+import os
+
+#def get_icon(icon):
 
 dunst_hist = subp.run(["dunstctl","history"], capture_output=True, text=True)
 #subp.run(["eww","update","get_noti=(box :space-evenly \"true\" :vexpand \"false\" :orientation \"v\" (label :text \"hello in shell\" :valign \"center\" :class \"weather_label\" )(label :text \"hello in shell\" :valign \"center\" :class \"weather_label\" ))"])
@@ -10,8 +13,32 @@ dunst_hist = subp.run(["dunstctl","history"], capture_output=True, text=True)
 #print(dunst_hist.stdout)
 dunst_json = json.loads(dunst_hist.stdout)
 #(label :text \"hello in shell\" :valign \"center\" :class \"weather_label\" )(label :text \"hello in shell\" :valign \"center\" :class \"weather_label\" )
-print(dunst_json["data"][0][1])
+#print(dunst_json["data"][0][1])
+final_string = ""
+button_script = "/home/branchmanager/.config/eww/scripts/toggle_fullQuote.sh reveal"
+for i in range(len(dunst_json["data"][0][0:10])):
+    #print(len(dunst_json["data"][0][0:3]))
+    #if(dunst_json["data"][0][i] != 0):
+    updated_button = button_script + str(i)
+    message = dunst_json["data"][0][i]["message"]["data"]
+    summary = dunst_json["data"][0][i]["summary"]["data"]
+    appname = dunst_json["data"][0][i]["appname"]["data"]
+    button_index = str(i)
+    #print(message + summary + appname)
 
+    icon_path = "/home/branchmanager/.config/eww/images/icons/"+appname+".svg"
+
+    if(not(os.path.exists(icon_path))):
+        icon_path = "/home/branchmanager/.config/eww/images/icons/notifbell.svg"
+
+    print(icon_path)
+    print(updated_button)
+
+    #print(exists)
+    #icon_class = appname+"_class"
+    #else:
+    #    print("nada")
+    
 '''for loop up until 4th notification
         check if it exists
             grab appname
@@ -20,4 +47,14 @@ print(dunst_json["data"][0][1])
             
             if appname has icon
                 get icon path using appname
+                maybe use a function here that returns the path along with the class name
+                the different icons will need to be scales accordingly
+            else 
+                use a generic bell icon or something
+        
+        using appname summary message and iconpath create notifcard[] eww widget fuunction as string
+        add this string to an appended string of notifcard widgets ex: "(nofifcard[:icon path :name appname :summ.....]) (notifcard[....])"
+            
+        //somewhere i need to check if the notification caome from a rondom app or one of the apps I have icosn for
+        //maybe somewhere add button arg too so we know what reveal bool to change   
             '''
--- .config/eww/scripts/toggle_fullQuote.sh
+++ .config/eww/scripts/toggle_fullQuote.sh
@@ -1,15 +1,15 @@
-full_quote_state=$(eww state | grep redd | grep true)
+full_quote_state=$(eww state | grep $1 | grep false)
 echo $full_quote_state
 
-#Above we check if the variable is set to true by checking for an empty variable and if it is true or the variable is empty
-#update the eww widget to false
+#Above we check if the variable is set to false by checking for an empty variable and if it is false or the variable is empty
+#update the eww widget to true
 if [ -z "$full_quote_state" ]
 then
-	eww update redd="true"
-	#eww update redd="true"
+	eww update $1="false"
+	eww update $1="false"
 else
-	eww update redd="false"
+	eww update $1="true"
 fi
 
-full_quote_state=$(eww state | grep redd )
-echo $full_quote_state
+#full_quote_state=$(eww state | grep redd )
+#echo $full_quote_state
